4b12c8abe6a6223822377484e69ab18220471b39
albaraa422 <albaraamostafa952@gmail.com>
2025-07-17 19:34:29 +0300

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,33 @@
+
+
+new=float(input("hello! are you new?if you are new so enter 1 if no so enter 2:"))
+
+if new==1:
+   mbob=(float(input("enter how many years you have:")))
+   kok=input("enter your name:")
+   passd=float(input("enter your password🫣:"))
+elif new==2:
+   choose=float(input("choose if you want to transfer money so inter 1 if want to get money so type 2:"))
+   if choose==1:
+      passd=float(input("Enter you password🫣:"))
+      if passd==4413 or 3332 or 5454 or 3245 or 5454 or 4435 or 4342 or 4545 or 1234:
+         bank=float(input("enter the money you want to transform🤑:"))
+         if bank<100000:
+            print("👍transform succsed$👍")
+         elif bank>100000:
+              print("sorry😔 the bank can't transform all these money you can transform by 10000 on 10000✌️")
+         else:
+                print("⛔an error accured⛔")
+   elif choose==2:
+      passd=float(input("enter you password🫣:"))
+      if passd==4413 or 3332 or 5454 or 3245 or 5454 or 4435 or 4342 or 4545 or 1234:
+         bank=float(input("enter the money you want to get🤑:"))
+         if bank<100000:
+              print("👍transform succsed$👍")
+         elif bank>100000:
+               print("sorry😔 the bank can't transform all these money you can get it by 10000 on 10000✌️")
+         else:
+                print("⛔an error accured⛔")
+      else: 
+            print("incorrect password")
+ 
